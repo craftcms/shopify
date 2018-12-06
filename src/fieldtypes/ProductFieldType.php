18290aa69d1aead0b5be89ced8951a401fedbd77
Niklas Maier <maier-niklas@t-online.de>
2018-12-07 00:39:31 +0100

    #2 fix(src.fieldtypes.ProductFieldType): use correct craft multiselect classes

--- src/fieldtypes/ProductFieldType.php
+++ src/fieldtypes/ProductFieldType.php
@@ -3,12 +3,11 @@
 namespace shopify\fieldtypes;
 
 use Craft;
-use craft\base\Field;
 use craft\base\ElementInterface;
-use craft\base\PreviewableFieldInterface;
+use craft\fields\BaseOptionsField;
 use shopify\Shopify;
 
-class ProductFieldType extends Field implements PreviewableFieldInterface
+class ProductFieldType extends BaseOptionsField
 {
     // Static Methods
     // =========================================================================
@@ -24,6 +23,68 @@ class ProductFieldType extends Field implements PreviewableFieldInterface
         return Craft::t('shopify', 'Shopify Product');
     }
 
+    /**
+     * @inheritdoc
+     */
+    protected function optionsSettingLabel(): string
+    {
+        return Craft::t('shopify', 'Multi-select Produkte');
+    }
+
+    // Public Methods
+    // =========================================================================
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        parent::init();
+        $this->multi = true;
+    }
+
+
+    /**
+     * @inheritdoc
+     */
+    public function getSettingsHtml()
+    {
+        if (empty($this->options)) {
+            // Give it a default row
+            $this->options = [['label' => '', 'value' => '']];
+        }
+
+        return Craft::$app->getView()->renderTemplateMacro('_includes/forms', 'editableTableField',
+            [
+                [
+                    'label' => $this->optionsSettingLabel(),
+                    'instructions' => Craft::t('app', 'Define the available options.'),
+                    'id' => 'options',
+                    'name' => 'options',
+                    'addRowLabel' => Craft::t('app', 'Add an option'),
+                    'cols' => [
+                        'label' => [
+                            'heading' => Craft::t('app', 'Option Label'),
+                            'type' => 'singleline',
+                            'autopopulate' => 'value'
+                        ],
+                        'value' => [
+                            'heading' => Craft::t('app', 'Value'),
+                            'type' => 'singleline',
+                            'class' => 'code'
+                        ],
+                        'default' => [
+                            'heading' => Craft::t('app', 'Default?'),
+                            'type' => 'checkbox',
+                            'radioMode' => !$this->multi,
+                            'class' => 'thin'
+                        ],
+                    ],
+                    'rows' => $this->getProducts()
+                ]
+            ]);
+    }
+
 
     /**
      * returns the template-partial an editor sees when editing plugin-content on a page
@@ -36,26 +97,30 @@ class ProductFieldType extends Field implements PreviewableFieldInterface
      */
     public function getInputHtml($value, ElementInterface $element = null): string
     {
+        return Craft::$app->getView()->renderTemplate('shopify/_select',
+            [
+                'name' => $this->handle,
+                'values' => $value,
+                'field' => $this,
+                'products' => $this->getProducts()
+            ]);
+    }
+
+
+    private function getProducts()
+    {
         $productOptions = array('limit' => 250);
         $products = Shopify::getInstance()->service->getProducts($productOptions);
 
-        $options = array();
+        $options = [];
         if($products) {
             foreach ($products as $product) {
                 $options[] = array(
                     'label' => $product['title'],
-                    'productId' => $product['id']
+                    'value' => $product['id']
                 );
             }
         }
-
-        return Craft::$app->getView()->renderTemplate('shopify/_select',
-            [
-                'name' => $this->handle,
-                'value' => $value,
-                'field' => $this,
-                'products' => $options
-            ]);
+        return $options;
     }
-
 }
