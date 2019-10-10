b62b2ae32363629d218e211b00791be5a1552b67
Niklas Maier <maier-niklas@t-online.de>
2019-10-10 23:09:40 +0200

    #7 feat(src.fieldtypes.ProductFieldType): reset to before last release
    also add data-normalization to use selected values as an array

--- src/fieldtypes/ProductFieldType.php
+++ src/fieldtypes/ProductFieldType.php
@@ -3,12 +3,22 @@
 namespace shopify\fieldtypes;
 
 use Craft;
+use craft\base\Field;
 use craft\base\ElementInterface;
-use craft\fields\BaseOptionsField;
+use craft\base\PreviewableFieldInterface;
 use shopify\Shopify;
 
-class ProductFieldType extends BaseOptionsField
+class ProductFieldType extends Field implements PreviewableFieldInterface
 {
+    /**
+     * @param $value
+     * @return mixed
+     */
+    public function normalizeValue($value, ElementInterface $element = null) {
+        if(is_array($value)) return $value;
+        return json_decode($value);
+    }
+
     // Static Methods
     // =========================================================================
 
@@ -22,70 +32,7 @@ class ProductFieldType extends BaseOptionsField
         //category is the filename inside ./translations/en/
         return Craft::t('shopify', 'Shopify Product');
     }
-
-    /**
-     * @inheritdoc
-     */
-    protected function optionsSettingLabel(): string
-    {
-        return Craft::t('shopify', 'Multi-select Produkte');
-    }
-
-    // Public Methods
-    // =========================================================================
-
-    /**
-     * @inheritdoc
-     */
-    public function init()
-    {
-        parent::init();
-        $this->multi = true;
-    }
-
-
-    /**
-     * @inheritdoc
-     */
-    public function getSettingsHtml()
-    {
-        if (empty($this->options)) {
-            // Give it a default row
-            $this->options = [['label' => '', 'value' => '']];
-        }
-
-        return Craft::$app->getView()->renderTemplateMacro('_includes/forms', 'editableTableField',
-            [
-                [
-                    'label' => $this->optionsSettingLabel(),
-                    'instructions' => Craft::t('app', 'Define the available options.'),
-                    'id' => 'options',
-                    'name' => 'options',
-                    'addRowLabel' => Craft::t('app', 'Add an option'),
-                    'cols' => [
-                        'label' => [
-                            'heading' => Craft::t('app', 'Option Label'),
-                            'type' => 'singleline',
-                            'autopopulate' => 'value'
-                        ],
-                        'value' => [
-                            'heading' => Craft::t('app', 'Value'),
-                            'type' => 'singleline',
-                            'class' => 'code'
-                        ],
-                        'default' => [
-                            'heading' => Craft::t('app', 'Default?'),
-                            'type' => 'checkbox',
-                            'radioMode' => !$this->multi,
-                            'class' => 'thin'
-                        ],
-                    ],
-                    'rows' => $this->getProducts()
-                ]
-            ]);
-    }
-
-
+    
     /**
      * returns the template-partial an editor sees when editing plugin-content on a page
      *
@@ -97,30 +44,25 @@ class ProductFieldType extends BaseOptionsField
      */
     public function getInputHtml($value, ElementInterface $element = null): string
     {
-        return Craft::$app->getView()->renderTemplate('shopify/_select',
-            [
-                'name' => $this->handle,
-                'values' => $value,
-                'field' => $this,
-                'products' => $this->getProducts()
-            ]);
-    }
-
-
-    private function getProducts()
-    {
-        $productOptions = array('limit' => 250);
-        $products = Shopify::getInstance()->service->getProducts($productOptions);
+        $products = Shopify::getInstance()->service->getProducts();
 
         $options = [];
         if($products) {
             foreach ($products as $product) {
                 $options[] = array(
                     'label' => $product['title'],
-                    'value' => $product['id']
+                    'productId' => $product['id']
                 );
             }
         }
-        return $options;
+
+        return Craft::$app->getView()->renderTemplate('shopify/_select',
+            [
+                'name' => $this->handle,
+                'value' => $value,
+                'field' => $this,
+                'products' => $options
+            ]);
     }
+
 }
