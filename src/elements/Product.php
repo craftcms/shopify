7d6fa1bd7abd859815d8be66bc2b771645fee274
Luke Holder <lukemh@gmail.com>
2022-10-24 20:56:03 +0800

    Cleanup

--- src/elements/Product.php
+++ src/elements/Product.php
@@ -605,8 +605,14 @@ class Product extends Element
                 return $this->getShopifyStatusHtml();
             case 'shopifyId':
                 return $this->$attribute;
+            case 'options':
+                return collect($this->getOptions())->map(function ($option) {
+                    return Html::tag('span', $option['name'], [
+                        'title' => $option['name'] . ' option values: ' . collect($option['values'])->join(', '),
+                    ]);
+                })->join(',&nbsp;');
             case 'tags':
-                return collect($this->tags)->map(function ($tag) {
+                return collect($this->getTags())->map(function ($tag) {
                     return Html::tag('div', $tag, [
                         'style' => 'margin-bottom: 2px;',
                         'class' => 'token',
