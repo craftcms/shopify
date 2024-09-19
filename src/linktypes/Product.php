206c465a9823404821f4b3c9bbbf2f20f7308545
Nathaniel Hammond <nathaniel@pixelandtonic.com>
2024-09-19 16:28:41 +0100

    tidy

--- src/linktypes/Product.php
+++ src/linktypes/Product.php
@@ -7,26 +7,30 @@
 
 namespace craft\shopify\linktypes;
 
-use craft\shopify\elements\Product as ProductElement;
 use craft\fields\linktypes\BaseElementLinkType;
-
+use craft\shopify\elements\Product as ProductElement;
 
 /**
  * Shopify Product link type.
  *
  * @author Pixel & Tonic, Inc. <support@pixelandtonic.com>
- * @since 5.2.0
+ * @since 5.3.0
  */
 class Product extends BaseElementLinkType
 {
+    /**
+     * @inheritdoc
+     */
     protected static function elementType(): string
     {
         return ProductElement::class;
     }
 
+    /**
+     * @inheritdoc
+     */
     public static function displayName(): string
     {
         return ProductElement::lowerDisplayName();
     }
-
 }
