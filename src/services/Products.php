6baa38299600e31c1e4b0eed0cb559ed4e6d9b72
Nathaniel Hammond <nathaniel@pixelandtonic.com>
2026-03-20 08:07:06 +0000

    #202 Protect again `null` `tags` even though this should not be the case

--- src/services/Products.php
+++ src/services/Products.php
@@ -137,7 +137,7 @@ class Products extends Component
             'publishedAt' => Db::prepareDateForDb($product['publishedAt']),
             'publishedOnCurrentPublication' => (bool)$product['publishedOnCurrentPublication'],
             'shopifyStatus' => $product['status'],
-            'tags' => $product['tags'],
+            'tags' => $product['tags'] ?? [],
             'templateSuffix' => $product['templateSuffix'],
             'updatedAt' => Db::prepareDateForDb($product['updatedAt']),
             'vendor' => $product['vendor'],
