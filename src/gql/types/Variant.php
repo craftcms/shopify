b903160b7f5603c8e9d13f72c31b3dc3f5f68eb6
Nathaniel Hammond <nathaniel@pixelandtonic.com>
2026-04-03 06:09:59 +0100

    fix cs

--- src/gql/types/Variant.php
+++ src/gql/types/Variant.php
@@ -12,7 +12,6 @@ use craft\gql\base\ObjectType;
 use craft\gql\GqlEntityRegistry;
 use craft\shopify\models\Variant as VariantElement;
 use craft\shopify\Plugin;
-use GraphQL\Type\Definition\ResolveInfo;
 use GraphQL\Type\Definition\Type;
 
 /**
@@ -116,11 +115,11 @@ class Variant extends ObjectType
                 'name' => 'metafields',
                 'type' => Type::listOf(Metafield::getType()),
                 'description' => 'Metafields of the variant.',
-                'resolve' => function (VariantElement $source) {
+                'resolve' => function(VariantElement $source) {
                     return collect($source->getMetafields())
                         ->map(fn($value, $key) => ['key' => $key, 'value' => $value])
                         ->all();
-                }
+                },
             ],
             'selectedOptions' => [
                 'name' => 'selectedOptions',
